9c67ae45adc7176e1383878c5d6e87acd74fbe78
isa <isa430752@gmail.com>
2023-06-25 19:07:43 +0300

    improvements

--- layout/js/backend.js
+++ layout/js/backend.js
@@ -1,5 +1,14 @@
 $(function()
 {
+  //Dashbord page
+  $('.toggle-info').click(function (){
+    $(this).toggleClass('selected').parent().next('.panel-body').fadeToggle(100);
+    if ($(this).hasClass('selected')){
+      $(this).html('<i class="fa fa-minus fa-lg"></i>')
+    }else {
+      $(this).html('<i class="fa fa-plus fa-lg"></i>')
+    }
+  })
   $('.confirm').click(function (){
     return  confirm('Are Your Sure...?');
   });
