6045ca7efafe12145219e60239d1a0af18441790
isa <isa430752@gmail.com>
2023-06-26 03:51:06 +0300

    Comments - Edit, Update and Delete

--- comments.php
+++ comments.php
@@ -54,11 +54,11 @@ if(isset($_SESSION['Username'])){
                             echo "<td>" . $comment['username']   . "</td>";
                             echo "<td>" . $comment['Comment_data']."</td>";
                             echo "<td>
-                              <a class='btn btn-success' href='commetns.php?do=Edit&comment_id=".$comment['comment_ID']."' role='button'><i class='fa-solid fa-pen-to-square'> </i> Edit </a>
-                              <a  href='commetns.php?do=Delete&comment_id=".$comment['comment_ID']."' role='button' class='btn btn-danger confirm' ><i class='fa-regular fa-trash-can'> </i> Delete </a>";
+                              <a class='btn btn-success' href='comments.php?do=Edit&comment_id=".$comment['comment_ID']."' role='button'><i class='fa-solid fa-pen-to-square'> </i> Edit </a>
+                              <a  href='comments.php?do=Delete&comment_id=".$comment['comment_ID']."' role='button' class='btn btn-danger confirm' ><i class='fa-regular fa-trash-can'> </i> Delete </a>";
                             if($comment['Status'] == 0 ){
 
-                                echo "<a  href='members.php?do=activate&ID=".$comment['comment_ID']."' role='button' class='btn btn-info confirm Activate' ><i class='fa-solid fa-circle-check''></i> Activate </a>";
+                                echo "<a  href='comments.php?do=activate&comment_id=".$comment['comment_ID']."' role='button' class='btn btn-info confirm Activate' ><i class='fa-solid fa-circle-check''></i> Activate </a>";
                             }
                             echo  "</td>";
                             echo "<tr>";
@@ -74,11 +74,11 @@ if(isset($_SESSION['Username'])){
 
         // Chek if  Get Request is numaric & Get the integer value of it
 
-        $commentid = isset($_GET['comment_ID'])&& is_numeric($_GET['comment_ID']) ? intval($_GET['comment_ID']) : 0 ;
+        $commentid = isset($_GET['comment_id'])&& is_numeric($_GET['comment_id']) ? intval($_GET['comment_id']) : 0 ;
 
         //Select all data Depend On this ID
 
-        $stetment = $connection->prepare("SELECT *  FROM comments  WHERE UserID = ?");
+        $stetment = $connection->prepare("SELECT *  FROM comments  WHERE comment_ID = ?");
 
         //Execute query
 
@@ -98,57 +98,65 @@ if(isset($_SESSION['Username'])){
                 <form class="contact-form" action="<?php echo "?do=Update" ?>" method="POST">
                     <!-- Start user name field -->
                     <i class="fa-solid fa-user icons"></i>
-                    <input type="hidden" name="userid" value="<?php echo $commentid ; ?>">
+                    <input type="hidden" name="commentid" value="<?php echo $commentid; ?>">
                     <div class="form-group">
                         <input
                             class="form-control"
                             type="text"
-                            name="username"
+                            name="comment"
                             placeholder="Type your iser name "
-                            value="<?php echo '     '.$row['Username'] ; ?>">
+                            value="<?php echo '     '.$row['Comment'] ; ?>">
                         <span class="axstrisx">*</span>
                     </div>
                     <!-- End username field -->
-                    <!-- Start Password  field -->
-                    <i class="fa-solid fa-lock icons"></i>
+                    <!-- Start members Field -->
                     <div class="form-group">
-                        <input
-                            class="form-control"
-                            type="hidden"
-                            name="oldpassword"
-                            value="<?php echo '     '.$row["Password"] ;?>">
-                        <input
-                            class="form-control"
-                            type="password"
-                            name="newpassword"
-                            placeholder="   Type Your New Password... "
-                            value="">
+                        <select  class="form-control selection " name="item_name">
+                            <option value="0">...</option>
+                            <?php
+                            $stetment=$connection->prepare("SELECT * FROM items");
+                            $stetment ->execute();
+                            $items =$stetment ->fetchAll();
+                            foreach ($items as $item ){
+                                echo  "<option value='".$item['Item_ID']."'";
+                                if($row['item_id'] == $item['Item_ID']){ echo "selected" ;}
+                                echo ">". $item['Name'] . "</option>";
+                            }
+                            ?>
+
+                        </select>
                     </div>
-                    <!-- End Password  field -->
-                    <!-- Start mail name field -->
-                    <i class="fa-solid fa-envelope icons"></i>
+                    <!-- End Catigore Field -->
+                    <!-- Start members Field -->
+                    <div class="form-group">
+                        <select  class="form-control selection " name="username">
+                            <option value="0">...</option>
+                            <?php
+                            $stetment=$connection->prepare("SELECT * FROM users");
+                            $stetment ->execute();
+                            $users =$stetment ->fetchAll();
+                            foreach ($users as $user ){
+                                echo  "<option value='".$user['UserID']."'";
+                                if($row['user_id'] == $user['UserID']){ echo "selected" ;}
+                                echo ">". $user['Username'] . "</option>";
+                            }
+                            ?>
+
+                        </select>
+                    </div>
+                    <!-- End Catigore Field -->
+                    <!-- Start user name field -->
+                    <i class="fa-solid fa-user icons"></i>
                     <div class="form-group">
                         <input
-                            class="form-control"
-                            type="email"
-                            name="email"
-                            placeholder="      Please Type a valid email"
-                            value="<?php echo '&nbsp &nbsp &nbsp '.$row['Email'] ; ?>">
+                                class="form-control"
+                                type="text"
+                                name="Comment_data"
+                                placeholder="Type your iser name "
+                                value="<?php echo '     '.$row['Comment_data'] ; ?>">
                         <span class="axstrisx">*</span>
                     </div>
-                    <!-- End mail name field -->
-                    <!-- Start Fullname  field -->
-                    <i class="fa-solid fa-user-group icons"></i>
-                    <div class="form-group">
-                        <input
-                            class="form-control"
-                            type="text"
-                            name="fullname"
-                            placeholder="      Type your fullname... "
-                            value="<?php echo '     '.$row['Fullname'] ; ?>">
-                        <span class="axstrisx">*</span>
-                    </div>
-                    <!-- End Fullname  field -->
+                    <!-- End username field -->
                     <!-- Start Save  field -->
                     <input
                         class="btn btn-success btn-block"
@@ -163,6 +171,7 @@ if(isset($_SESSION['Username'])){
             $errormsg = "Sorry You Enter a user ID that does not exist";
             redirect_home($errormsg);
         }
+
     }elseif($do == "Update"){// Start The Update page....
 
         echo      "<h1 class='text-center'>". lang("EditMemper")."</h1>";
@@ -170,53 +179,36 @@ if(isset($_SESSION['Username'])){
         if($_SERVER['REQUEST_METHOD'] == 'POST'){
 
             //Get  Variabels from the from....
-            $userid     = $_POST['userid'];
+            $commentid     = $_POST['commentid'];
+            $comment = filter_var($_POST['comment'],FILTER_SANITIZE_STRING);
+            $item_name = ($_POST['item_name']);
+            $username = $_POST['username'];
+            $Comment_data = $_POST['Comment_data'];
 
-            $user = filter_var($_POST['username'],FILTER_SANITIZE_STRING);
 
-            $email = filter_var($_POST ['email'],FILTER_SANITIZE_EMAIL);
 
-            $full  = filter_var($_POST['fullname'],FILTER_SANITIZE_STRING);
-
-            //The password Trik....
-            // if condition....
-            $pass = empty($_POST['newpassword']) ? $pass = $_POST['oldpassword'] : sha1($_POST['newpassword']);
 
             //Start Check the data sent....
             //Creatinh Array of errors
             $formNameError = array() ;
 
-            if(empty($user)){
-
-                $formNameError[] = "Username Cant Be Empty..!";
-
+            if(empty($comment)){
+                $fromError [] = 'comment Can\'t be <sronge>Empty</sronge>';
             }
-            if(strlen($user) < 3 ){
-
-                $formNameError[] = "Username Nust  be Lareger Than <strong>5</strong> Characters..!";
-
+            if(empty($username)){
+                $fromError [] = 'username Can\'t be <sronge>Empty</sronge>';
             }
-            if(strlen($pass) < 2  ){
-
-                $formNameError[] = "cellnumper Nust be Larger Then <strong>8</strong> Numper..!";
-
+            if(empty($item_name)){
+                $fromError [] = 'item_name Can\'t be <sronge>Empty</sronge>';
             }
-            if(empty($full) ){
-
-                $formNameError[] = "Fullname Cant Be Empty..!";
-
-            }
-            if(strlen($full) <= 1 ){
-
-                $formNameError[] = "Message Nust be Lareger Then  <strong> 10</strong> Characters..!";
-
+            if(empty($Comment_data)){
+                $fromError [] = 'Comment_data Can\'t be <sronge>Empty</sronge>';
             }
             if( ! empty($formNameError)) { ?>
 
                 <div class="alert alert-danger" role="alert">
 
                     <?php
-                    // header('Location:members.php?do=Edit&ID=?');
                     foreach($formNameError as $errors){
                         echo $errors. '<br>';
                     }
@@ -227,9 +219,13 @@ if(isset($_SESSION['Username'])){
             //If there are no errors, perform the update process
             else{
                 //Update the  database with This Information
-                $stetment = $connection->prepare("UPDATE users SET Username = ? , Email = ? , Fullname = ? , Password = ? WHERE UserID = ?");
-                $stetment->execute(array($user,$email,$full,$pass,$userid));
-                echo $stetment->rowCount()." Record updated...";
+                $stetment = $connection->prepare("UPDATE comments SET Comment = ? , item_id = ? , user_id = ? , Comment_data = ? WHERE comment_ID = ?");
+                $stetment->execute(array($comment,$item_name,$username,$Comment_data,$commentid));
+                echo '<div class="container">' ;
+                $themesg     ='<div class=" alert alert-info">'.$stetment->rowCount().'Record updated...</div>' ;
+                $page_adress = 'comments.php';
+                redirect_home($themesg,2,$page_adress);
+                echo '</div>';
             }
         }else{ // error page
             $errormsg = "Sorry You Cant Browse Tihs Page Directry";
@@ -240,11 +236,11 @@ if(isset($_SESSION['Username'])){
         // Chek if  Get Request is numeric & Get the integer value of it
         $commentid = isset($_GET['comment_id'])&& is_numeric($_GET['comment_id']) ? intval($_GET['comment_id']) : 0 ;
 
-        $chek = chekitem('comment_Id' , 'commetns' , $commentid);
+        $chek = chekitem('comment_Id' , 'comments' , $commentid );
 
         if($chek > 0 ){
 
-            $stetment = $connection->prepare("DELETE FROM commetns WHERE comment_ID = ?");
+            $stetment = $connection->prepare("DELETE FROM comments WHERE comment_ID = ?");
             $stetment ->execute(array($commentid));
             // Redirect to another page
             echo '<div class="container">' ;
@@ -263,16 +259,16 @@ if(isset($_SESSION['Username'])){
     }elseif ($do == "activate"){ //start User activation page
 
         // Chek if  Get Request is numeric & Get the integer value of it
-        $userid = isset($_GET['ID'])&& is_numeric($_GET['ID']) ? intval($_GET['ID']) : 0 ;
+        $commentid = isset($_GET['comment_id'])&& is_numeric($_GET['comment_id']) ? intval($_GET['comment_id']) : 0 ;
         //Verify that the user number exists IN database
-        $chek =chekitem('UserID' , 'users' ,  $userid);
+        $chek =chekitem('comment_ID' , 'comments' ,$commentid);
         if($chek > 0 ){
-            $stetment=$connection->prepare("UPDATE users SET RegStatus = 1 WHERE UserID = ?");
-            $stetment->execute(array($userid));
+            $stetment=$connection->prepare("UPDATE comments SET Status = 1 WHERE comment_ID = ?");
+            $stetment->execute(array($commentid));
             // Redirect to another page
             echo '<div class="container">' ;
 
-            $page_adress = 'members.php';
+            $page_adress = 'comments.php';
 
             $themesg ='<div class="alert alert-success">Activation completed successfully...!</div>';
 
@@ -282,9 +278,9 @@ if(isset($_SESSION['Username'])){
         }else{
 
             echo '<div class="container">' ;
-            $page_adress = 'members.php';
+            $page_adress = 'comments.php';
             $themesg ='<div class="alert alert-danger">We do not have this user in our records...!</div>';
-            redirect_home($themesg,1,$page_adress);
+            redirect_home($themesg,3,$page_adress);
             echo '</div>' ;
         }
 
